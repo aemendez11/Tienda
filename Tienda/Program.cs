08e1ef96fa9b4e84034a70dcfec3cc31331a6954
LENOVO <LENOVO@LAPTOP-P1U8KGS8>
2024-05-15 23:24:17 -0600

    editar productos

--- Tienda/Program.cs
+++ Tienda/Program.cs
@@ -6,7 +6,9 @@ using Tienda;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddBlazoredLocalStorage();
+
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazoredLocalStorage();
 
 await builder.Build().RunAsync();
